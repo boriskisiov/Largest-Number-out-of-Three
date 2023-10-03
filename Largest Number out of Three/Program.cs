50af1ca4ef35a57458048ec87422fb46ce3695c6
boriskisiov <boriskisiov@gmail.com>
2023-10-03 12:32:13 +0300

    Add project files.

---
+++ Largest Number out of Three/Program.cs
@@ -0,0 +1,16 @@
+﻿int first = int.Parse(Console.ReadLine());
+int second = int.Parse(Console.ReadLine());
+int third = int.Parse(Console.ReadLine());
+
+if (first > second && first > third)
+{
+    Console.WriteLine("First");
+}
+else if (second > first && second > third)
+{
+    Console.WriteLine("Second");
+}
+else if (third > first && third > second)
+{
+    Console.WriteLine("Third");
+}
